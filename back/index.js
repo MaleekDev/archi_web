57a3176ab64ca7f569c50b821242cf2ad7739492
MaleekDev <maleek.hodari@gmail.com>
2021-04-28 17:45:17 +0200

    mis a jour 28.04.2021

--- back/index.js
+++ back/index.js
@@ -6,19 +6,25 @@ const cors = require('cors')
 
 
 app.use(cors())
+
 /* application  */
 app.get('/evenement', (req, res) => {
   res.json ([{
         id: "abc1",
+        scene: "1",
         titre: "Titre exemple 1",
         dateHeureDebut: "15, 04, 2021",
-        dateHeureFin: new Date()
+        dateHeureFin: new Date(),
+        details: "Ce matin toute les séances seront en pleine air"
     },
     {
+        
         id: "ztp2",
+        scene: "2",
         titre: "Titre exemple 2",
         dateHeureDebut: "30, 04, 2021",
-        dateHeureFin: new Date()
+        dateHeureFin: new Date(),
+        details: "Ce soir toute les séances seront en pleine air"
     }]);
 
 });
--- front/index.js
+++ front/index.js
@@ -1,3 +1,52 @@
+// let tabEvenement = []
+// /*************** Evenement afficher **********************/
+// function afficherEvenement(evenement) {
+//     document.getElementById("listeEvenement").innerHTML += `
+//     <div class="card" style="background:#cfdaf8; padding: 50px; margin:80px;" id="bloc-ev-${evenement.id}">
+//         <h1> Titre : ${evenement.titre}</h1>
+//         <p class="date"> Début ${evenement.dateHeureDebut}</p>
+//         <p class="date"> Fin ${evenement.dateHeureFin}</p>
+//         <p> Détaila donné ${evenement.dateHeureFin}</p>
+//         <button onclick='supprimerEvenement("${evenement.id}")' type="button" class="btn btn-dark">Supprimer</button>
+//         <button onclick='afficherDetails("${evenement.details}")' type="button" class="btn btn-link">Avoir plus de renseignement</button>
+//         <h1></h1>
+//     </div>`;
+// } 
+
+// function afficherDetails(detailsEvenement) {
+//     console.log(tabEvenement);
+//     console.log(detailsEvenement);
+
+//     let detail = tabEvenement.find(ev => ev.details === detailsEvenement);
+//     let afficheDetail = detail;
+//     alert (afficheDetail);
+// }
+
+// /*************** Evenement suppression ******************/
+// function supprimerEvenement(id) {
+//     document.getElementById(`bloc-ev-${id}`).remove();
+// }
+// /*************** end ******************/
+
+// /************** boucle du bouton pour afficher l'evenement *****************/
+// function traiterSuccessFetch(affichee){
+//     /* console.log(titre, debut); */
+//     for (let ev of affichee) {
+//         afficherEvenement(ev);
+//         tabEvenement.push(ev);
+//     }
+// }
+// /************** end *****************/
+
+// function traiterErreurFetch(erreur) {
+//     console.log("erreur");
+// }
+
+// document.getElementById("monBouton").addEventListener('click', function() {
+//     fetch("http://localhost:3000/evenement")
+//         .then(res => res.json())
+//         .then(traiterSuccessFetch, traiterErreurFetch);
+// });
 /*************** Evenement afficher **********************/
 function afficherEvenement(evenement) {
     document.getElementById("listeEvenement").innerHTML += `
